df4ad33a95903aa830d50e5795f240839fa3d871
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-10-24 10:38:08 +0700

    ff

---
+++ src/components/pages/lenh-can/MainCreateService/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface PropsMainCreateService {}
+
+export interface IFormCreateService {
+	shipUuid: string;
+	transportType: number;
+	isPrint: number;
+	productTypeUuid: string;
+	timeIntend: string | Date;
+	weightIntent: number;
+	documentId: string;
+	description: string;
+	customerUuid: string;
+	scaleStationUuid: string;
+	portname: string;
+}
