3b5a203e25b119f0d7905d67db8aea4bba9a5aa9
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-11-07 15:58:48 +0700

    ff

--- src/components/pages/nhap-xuat-ngoai/MainCreateImport/MainCreateImport.tsx
+++ src/components/pages/nhap-xuat-ngoai/MainCreateImport/MainCreateImport.tsx
@@ -1,10 +1,442 @@
-import React from "react"; 
+import React, {useState} from 'react';
 
-import {PropsMainCreateImport} from "./interfaces"; 
-import styles from "./MainCreateImport.module.scss"; 
+import {IFormCreateImport, PropsMainCreateImport} from './interfaces';
+import styles from './MainCreateImport.module.scss';
+import TextArea from '~/components/common/Form/components/TextArea';
+import Form, {FormContext, Input} from '~/components/common/Form';
+import Select, {Option} from '~/components/common/Select';
+import Button from '~/components/common/Button';
+import Loading from '~/components/common/Loading';
+import {clsx} from 'clsx';
+import {useRouter} from 'next/router';
+import {
+	CONFIG_DESCENDING,
+	CONFIG_PAGING,
+	CONFIG_STATUS,
+	CONFIG_TYPE_FIND,
+	QUERY_KEY,
+	STATUS_CUSTOMER,
+	TYPE_BATCH,
+	TYPE_CUSTOMER,
+	TYPE_SCALES,
+} from '~/constants/config/enum';
+import customerServices from '~/services/customerServices';
+import {httpRequest} from '~/services';
+import {useMutation, useQuery} from '@tanstack/react-query';
+import {IDetailCustomer} from '../../lenh-can/MainCreateImport/interfaces';
+import storageServices from '~/services/storageServices';
+import warehouseServices from '~/services/warehouseServices';
+import DatePicker from '~/components/common/DatePicker';
+import batchBillServices from '~/services/batchBillServices';
+import {price} from '~/common/funcs/convertCoin';
+import moment from 'moment';
+import UploadMultipleFile from '~/components/common/UploadMultipleFile';
+import {toastWarn} from '~/common/funcs/toast';
+import uploadImageService from '~/services/uploadService';
 
-function MainCreateImport({}:PropsMainCreateImport) { 
-	return <div>MainCreateImport</div>; 
-} 
+function MainCreateImport({}: PropsMainCreateImport) {
+	const router = useRouter();
+	const [loading, setLoading] = useState<boolean>(false);
+	const [images, setImages] = useState<any[]>([]);
+
+	const [form, setForm] = useState<IFormCreateImport>({
+		weightIntent: 0,
+		specificationsUuid: '',
+		warehouseUuid: '',
+		productTypeUuid: '',
+		description: '',
+		fromUuid: '',
+		toUuid: '',
+		timeStart: new Date(),
+	});
+
+	const {data: detailCustomer} = useQuery<IDetailCustomer>([QUERY_KEY.chi_tiet_khach_hang, form.fromUuid], {
+		queryFn: () =>
+			httpRequest({
+				http: customerServices.getDetail({
+					uuid: form.fromUuid,
+				}),
+			}),
+		onSuccess(data) {
+			if (data) {
+				const listspecUu: any[] = [...new Map(data?.customerSpec?.map((v: any) => [v?.specUu?.uuid, v])).values()];
+				const listProductTypeUu: any[] = [...new Map(data?.customerSpec?.map((v: any) => [v?.productTypeUu?.uuid, v])).values()];
+
+				setForm((prev) => ({
+					...prev,
+					specificationsUuid: listspecUu?.[0]?.specUu?.uuid || '',
+					productTypeUuid: listProductTypeUu?.[0]?.productTypeUu?.uuid || '',
+				}));
+			}
+		},
+		select(data) {
+			return data;
+		},
+		enabled: !!form.fromUuid,
+	});
+
+	const listCustomer = useQuery([QUERY_KEY.dropdown_khach_hang], {
+		queryFn: () =>
+			httpRequest({
+				isDropdown: true,
+				http: customerServices.listCustomer({
+					page: 1,
+					pageSize: 50,
+					keyword: '',
+					isPaging: CONFIG_PAGING.NO_PAGING,
+					isDescending: CONFIG_DESCENDING.NO_DESCENDING,
+					typeFind: CONFIG_TYPE_FIND.TABLE,
+					partnerUUid: '',
+					userUuid: '',
+					status: STATUS_CUSTOMER.HOP_TAC,
+					typeCus: TYPE_CUSTOMER.NHA_CUNG_CAP,
+					provinceId: '',
+					specUuid: '',
+				}),
+			}),
+		select(data) {
+			return data;
+		},
+	});
+
+	const listWarehouse = useQuery([QUERY_KEY.dropdown_kho_hang], {
+		queryFn: () =>
+			httpRequest({
+				isDropdown: true,
+				http: warehouseServices.listWarehouse({
+					page: 1,
+					pageSize: 50,
+					keyword: '',
+					status: CONFIG_STATUS.HOAT_DONG,
+					isPaging: CONFIG_PAGING.NO_PAGING,
+					isDescending: CONFIG_DESCENDING.NO_DESCENDING,
+					typeFind: CONFIG_TYPE_FIND.TABLE,
+					customerUuid: '',
+					timeEnd: null,
+					timeStart: null,
+				}),
+			}),
+		select(data) {
+			return data;
+		},
+	});
+
+	const listStorage = useQuery([QUERY_KEY.dropdown_bai, form.specificationsUuid, form.productTypeUuid, form.warehouseUuid], {
+		queryFn: () =>
+			httpRequest({
+				isDropdown: true,
+				http: storageServices.listStorage({
+					page: 1,
+					pageSize: 50,
+					keyword: '',
+					status: CONFIG_STATUS.HOAT_DONG,
+					isPaging: CONFIG_PAGING.NO_PAGING,
+					isDescending: CONFIG_DESCENDING.NO_DESCENDING,
+					typeFind: CONFIG_TYPE_FIND.DROPDOWN,
+					qualityUuid: '',
+					specificationsUuid: form.specificationsUuid,
+					warehouseUuid: form.warehouseUuid,
+					productUuid: form.productTypeUuid,
+				}),
+			}),
+		onSuccess(data) {
+			if (data) {
+				setForm((prev) => ({
+					...prev,
+					toUuid: data?.[0]?.uuid || '',
+				}));
+			}
+		},
+		select(data) {
+			return data;
+		},
+		enabled: !!form.warehouseUuid && !!form.productTypeUuid && !!form.specificationsUuid,
+	});
+
+	const funcCreatBill = useMutation({
+		mutationFn: (body: {paths: string[]}) =>
+			httpRequest({
+				showMessageFailed: true,
+				showMessageSuccess: true,
+				msgSuccess: 'Thêm mới thành công!',
+				http: batchBillServices.upsertBillNoScales({
+					batchUuid: '',
+					billUuid: '',
+					shipOutUuid: '',
+					customerName: '',
+					isCreateBatch: 1,
+					shipUuid: '',
+					timeIntend: null,
+					weightIntent: price(form?.weightIntent),
+					isBatch: TYPE_BATCH.KHONG_CAN,
+					isSift: null,
+					scalesType: TYPE_SCALES.CAN_NHAP,
+					specificationsUuid: form.specificationsUuid,
+					productTypeUuid: form.productTypeUuid,
+					documentId: '',
+					description: form.description,
+					fromUuid: form.fromUuid,
+					toUuid: form?.toUuid,
+					isPrint: null,
+					transportType: null,
+					lstTruckAddUuid: [],
+					lstTruckRemoveUuid: [],
+					scaleStationUuid: '',
+					portname: '',
+					descriptionWs: '',
+					paths: body.paths,
+					timeEnd: null,
+					timeStart: form?.timeStart ? moment(form?.timeStart!).format('YYYY-MM-DD') : null,
+				}),
+			}),
+		onSuccess(data) {
+			if (data) {
+				router.back();
+			}
+		},
+		onError(error) {
+			console.log({error});
+			return;
+		},
+	});
+
+	const handleSubmit = async () => {
+		const imgs = images?.map((v: any) => v?.file);
+
+		if (imgs.length > 0) {
+			const dataImage = await httpRequest({
+				setLoading,
+				isData: true,
+				http: uploadImageService.uploadMutilImage(imgs),
+			});
+			if (dataImage?.error?.code == 0) {
+				return funcCreatBill.mutate({
+					paths: dataImage.items,
+				});
+			} else {
+				return toastWarn({msg: 'Upload ảnh thất bại!'});
+			}
+		} else {
+			return funcCreatBill.mutate({
+				...form,
+				paths: [],
+			});
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<Loading loading={loading || funcCreatBill.isLoading} />
+			<Form form={form} setForm={setForm} onSubmit={handleSubmit}>
+				<div className={styles.header}>
+					<div className={styles.left}>
+						<h4>Thêm lệnh cân nhập dự kiến</h4>
+						<p>Điền đầy đủ các thông tin phiếu nhập hàng</p>
+					</div>
+					<div className={styles.right}>
+						<Button p_10_24 rounded_2 grey_outline onClick={() => router.back()}>
+							Hủy bỏ
+						</Button>
+						<FormContext.Consumer>
+							{({isDone}) => (
+								<Button disable={!isDone} p_10_24 rounded_2 primary>
+									Lưu lại
+								</Button>
+							)}
+						</FormContext.Consumer>
+					</div>
+				</div>
+				<div className={styles.form}>
+					<div className={clsx('mt')}>
+						<Select
+							isSearch
+							name='fromUuid'
+							placeholder='Chọn nhà cung cấp'
+							value={form?.fromUuid}
+							label={
+								<span>
+									Nhà cung cấp <span style={{color: 'red'}}>*</span>
+								</span>
+							}
+						>
+							{listCustomer?.data?.map((v: any) => (
+								<Option
+									key={v?.uuid}
+									value={v?.uuid}
+									title={v?.name}
+									onClick={() =>
+										setForm((prev: any) => ({
+											...prev,
+											fromUuid: v?.uuid,
+											transportType: v?.transportType,
+											isSift: v?.isSift,
+											productTypeUuid: '',
+											specificationsUuid: '',
+											shipUuid: '',
+										}))
+									}
+								/>
+							))}
+						</Select>
+					</div>
+
+					<div className={clsx('mt', 'col_2')}>
+						<Select
+							isSearch
+							name='productTypeUuid'
+							placeholder='Chọn loại hàng'
+							value={form?.productTypeUuid}
+							label={
+								<span>
+									Loại hàng<span style={{color: 'red'}}>*</span>
+								</span>
+							}
+						>
+							{[...new Map(detailCustomer?.customerSpec?.map((v: any) => [v?.productTypeUu?.uuid, v])).values()]?.map(
+								(v: any) => (
+									<Option
+										key={v?.uuid}
+										value={v?.productTypeUu?.uuid}
+										title={v?.productTypeUu?.name}
+										onClick={() =>
+											setForm((prev: any) => ({
+												...prev,
+												productTypeUuid: v?.productTypeUu?.uuid,
+												toUuid: '',
+											}))
+										}
+									/>
+								)
+							)}
+						</Select>
+						<div>
+							<Select
+								isSearch
+								name='specificationsUuid'
+								placeholder='Chọn quy cách'
+								value={form?.specificationsUuid}
+								label={
+									<span>
+										Quy cách <span style={{color: 'red'}}>*</span>
+									</span>
+								}
+							>
+								{[...new Map(detailCustomer?.customerSpec?.map((v: any) => [v?.specUu?.uuid, v])).values()]?.map(
+									(v: any) => (
+										<Option
+											key={v?.specUu?.uuid}
+											value={v?.specUu?.uuid}
+											title={v?.specUu?.name}
+											onClick={() =>
+												setForm((prev: any) => ({
+													...prev,
+													specificationsUuid: v?.specUu?.uuid,
+													toUuid: '',
+												}))
+											}
+										/>
+									)
+								)}
+							</Select>
+						</div>
+					</div>
+					<div className={clsx('mt', 'col_2')}>
+						<Select
+							isSearch
+							name='warehouseUuid'
+							placeholder='Chọn kho hàng chính'
+							value={form?.warehouseUuid}
+							label={
+								<span>
+									Kho hàng chính <span style={{color: 'red'}}>*</span>
+								</span>
+							}
+						>
+							{listWarehouse?.data?.map((v: any) => (
+								<Option
+									key={v?.uuid}
+									value={v?.uuid}
+									title={v?.name}
+									onClick={() =>
+										setForm((prev: any) => ({
+											...prev,
+											warehouseUuid: v?.uuid,
+											toUuid: '',
+											// scaleStationUuid: v?.scaleStationUu?.uuid || '',
+										}))
+									}
+								/>
+							))}
+						</Select>
+						<div>
+							<Select
+								isSearch
+								name='toUuid'
+								placeholder='Chọn bãi'
+								value={form?.toUuid}
+								readOnly={!form.warehouseUuid || !form.productTypeUuid || !form.specificationsUuid}
+								label={
+									<span>
+										Bãi <span style={{color: 'red'}}>*</span>
+									</span>
+								}
+							>
+								{listStorage?.data?.map((v: any) => (
+									<Option
+										key={v?.uuid}
+										value={v?.uuid}
+										title={v?.name}
+										onClick={() =>
+											setForm((prev: any) => ({
+												...prev,
+												toUuid: v?.uuid,
+											}))
+										}
+									/>
+								))}
+							</Select>
+						</div>
+					</div>
+					<div className={clsx('mt', 'col_2')}>
+						<Input
+							name='weightIntent'
+							value={form.weightIntent || ''}
+							type='text'
+							isMoney
+							unit='KG'
+							label={<span>Khối lượng hàng</span>}
+							placeholder='Nhập khối lượng hàng'
+						/>
+						<div>
+							<DatePicker
+								label={
+									<span>
+										Ngày nhập hàng <span style={{color: 'red'}}>*</span>
+									</span>
+								}
+								value={form.timeStart}
+								onSetValue={(date) =>
+									setForm((prev: any) => ({
+										...prev,
+										timeStart: date,
+									}))
+								}
+								placeholder='Chọn ngày nhập hàng'
+							/>
+						</div>
+					</div>
+
+					<div className={clsx('mt')}>
+						<TextArea name='description' placeholder='Nhập ghi chú' max={5000} blur label={<span>Ghi chú</span>} />
+					</div>
+					<div className='mt'>
+						<div className={styles.image_upload}>Chọn ảnh</div>
+						<UploadMultipleFile images={images} setImages={setImages} />
+					</div>
+				</div>
+			</Form>
+		</div>
+	);
+}
 
 export default MainCreateImport;
--- src/components/pages/nhap-xuat-ngoai/MainPageDirect/MainPageDirect.tsx
+++ src/components/pages/nhap-xuat-ngoai/MainPageDirect/MainPageDirect.tsx
@@ -1,10 +1,297 @@
-import React from "react"; 
+import React from 'react';
 
-import {PropsMainPageDirect} from "./interfaces"; 
-import styles from "./MainPageDirect.module.scss"; 
+import {PropsMainPageDirect} from './interfaces';
+import styles from './MainPageDirect.module.scss';
+import DataWrapper from '~/components/common/DataWrapper';
+import Pagination from '~/components/common/Pagination';
+import {Eye} from 'iconsax-react';
+import IconCustom from '~/components/common/IconCustom';
+import {LuPencil} from 'react-icons/lu';
+import Moment from 'react-moment';
+import {convertWeight} from '~/common/funcs/optionConvert';
+import {
+	CONFIG_DESCENDING,
+	CONFIG_PAGING,
+	CONFIG_TYPE_FIND,
+	QUERY_KEY,
+	STATE_BILL,
+	STATUS_BILL,
+	TYPE_BATCH,
+	TYPE_DATE,
+	TYPE_SCALES,
+	TYPE_TRANSPORT,
+} from '~/constants/config/enum';
+import Link from 'next/link';
+import Table from '~/components/common/Table';
+import Noti from '~/components/common/DataWrapper/components/Noti';
+import Button from '~/components/common/Button';
+import Image from 'next/image';
+import DateRangerCustom from '~/components/common/DateRangerCustom';
+import Search from '~/components/common/Search';
+import {useRouter} from 'next/router';
+import icons from '~/constants/images/icons';
+import batchBillServices from '~/services/batchBillServices';
+import {httpRequest} from '~/services';
+import {useQuery} from '@tanstack/react-query';
 
-function MainPageDirect({}:PropsMainPageDirect) { 
-	return <div>MainPageDirect</div>; 
-} 
+function MainPageDirect({}: PropsMainPageDirect) {
+	const router = useRouter();
+
+	const {
+		_page,
+		_pageSize,
+		_keyword,
+		_dateFrom,
+		_dateTo,
+		_isBatch,
+		_state,
+		_status,
+		_customerUuid,
+		_productTypeUuid,
+		_shipUuid,
+		_storageUuid,
+		_scalesStationUuid,
+	} = router.query;
+
+	const ListBill = useQuery(
+		[
+			QUERY_KEY.table_phieu_can_tat_ca,
+			_page,
+			_pageSize,
+			_keyword,
+			_isBatch,
+			_customerUuid,
+			_productTypeUuid,
+			_shipUuid,
+			_status,
+			_dateFrom,
+			_dateTo,
+			_state,
+			_storageUuid,
+			_scalesStationUuid,
+		],
+		{
+			queryFn: () =>
+				httpRequest({
+					isList: true,
+					http: batchBillServices.getListBill({
+						page: Number(_page) || 1,
+						pageSize: Number(_pageSize) || 50,
+						keyword: (_keyword as string) || '',
+						isPaging: CONFIG_PAGING.IS_PAGING,
+						isDescending: CONFIG_DESCENDING.NO_DESCENDING,
+						typeFind: CONFIG_TYPE_FIND.TABLE,
+						scalesType: [TYPE_SCALES.CAN_TRUC_TIEP],
+						state: !!_state
+							? [Number(_state)]
+							: [
+									STATE_BILL.NOT_CHECK,
+									STATE_BILL.QLK_REJECTED,
+									STATE_BILL.QLK_CHECKED,
+									STATE_BILL.KTK_REJECTED,
+									STATE_BILL.KTK_CHECKED,
+									STATE_BILL.END,
+							  ],
+						customerUuid: (_customerUuid as string) || '',
+						isBatch: TYPE_BATCH.KHONG_CAN,
+						isCreateBatch: null,
+						productTypeUuid: (_productTypeUuid as string) || '',
+						specificationsUuid: '',
+						status: !!_status
+							? [Number(_status)]
+							: [
+									STATUS_BILL.DANG_CAN,
+									STATUS_BILL.TAM_DUNG,
+									STATUS_BILL.DA_CAN_CHUA_KCS,
+									STATUS_BILL.DA_KCS,
+									STATUS_BILL.CHOT_KE_TOAN,
+							  ],
+						timeStart: _dateFrom ? (_dateFrom as string) : null,
+						timeEnd: _dateTo ? (_dateTo as string) : null,
+						warehouseUuid: '',
+						qualityUuid: '',
+						transportType: null,
+						shipUuid: (_shipUuid as string) || '',
+						typeCheckDay: 0,
+						scalesStationUuid: (_scalesStationUuid as string) || '',
+						storageUuid: (_storageUuid as string) || '',
+					}),
+				}),
+			select(data) {
+				return data;
+			},
+		}
+	);
+
+	const handleExportExcel = () => {
+		// return exportExcel.mutate();
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<div className={styles.main_search}>
+					<div className={styles.search}>
+						<Search keyName='_keyword' placeholder='Tìm kiếm theo số phiếu và mã lô hàng' />
+					</div>
+
+					<div className={styles.filter}>
+						<DateRangerCustom titleTime='Thời gian' typeDateDefault={TYPE_DATE.TODAY} />
+					</div>
+				</div>
+
+				<div className={styles.btn}>
+					<Button rounded_2 w_fit p_8_16 green bold onClick={handleExportExcel}>
+						Xuất excel
+					</Button>
+					<div>
+						<Button
+							rounded_2
+							w_fit
+							p_8_16
+							bold
+							href={'/nhap-xuat-ngoai/them-moi-xuat-thang'}
+							icon={<Image alt='icon add' src={icons.add} width={20} height={20} />}
+						>
+							Tạo phiếu
+						</Button>
+					</div>
+				</div>
+			</div>
+
+			<div className={styles.table}>
+				<DataWrapper data={ListBill?.data?.items || []} noti={<Noti des='Hiện tại chưa có dữ liệu nào!' disableButton />}>
+					<Table
+						data={ListBill?.data?.items || []}
+						column={[
+							{
+								title: 'STT',
+								render: (data: any, index: number) => <>{index + 1}</>,
+							},
+
+							{
+								title: 'Mã lô',
+								fixedLeft: true,
+								render: (data: any) => (
+									<Link href={`/phieu-can/${data.uuid}`} className={styles.link}>
+										{data?.code}
+									</Link>
+								),
+							},
+							{
+								title: 'Loại cân',
+								render: (data: any) => (
+									<p style={{fontWeight: 600}}>
+										{data?.scalesType == TYPE_SCALES.CAN_NHAP && 'Cân nhập'}
+										{data?.scalesType == TYPE_SCALES.CAN_XUAT && 'Cân xuất'}
+										{data?.scalesType == TYPE_SCALES.CAN_DICH_VU && 'Cân dịch vụ'}
+										{data?.scalesType == TYPE_SCALES.CAN_CHUYEN_KHO && 'Cân chuyển kho'}
+										{data?.scalesType == TYPE_SCALES.CAN_TRUC_TIEP && 'Cân xuất thẳng'}
+									</p>
+								),
+							},
+							{
+								title: 'Vận chuyển',
+								render: (data: any) => (
+									<>
+										{data?.transportType == TYPE_TRANSPORT.DUONG_BO && 'Đường bộ'}
+										{data?.transportType == TYPE_TRANSPORT.DUONG_THUY && 'Đường thủy'}
+									</>
+								),
+							},
+							{
+								title: 'Từ (tàu/xe)',
+								render: (data: any) => (
+									<>
+										<p style={{marginBottom: 4, fontWeight: 600}}>{data?.fromUu?.name || data?.customerName}</p>
+										{data?.isBatch == TYPE_BATCH.CAN_LO && (
+											<p style={{fontWeight: 600, color: '#3772FF'}}>
+												{data?.batchsUu?.shipUu?.licensePalate || '---'}
+											</p>
+										)}
+										{data?.isBatch == TYPE_BATCH.CAN_LE && (
+											<p style={{fontWeight: 600, color: '#3772FF'}}>
+												{data?.weightSessionUu?.truckUu?.licensePalate || '---'}
+											</p>
+										)}
+									</>
+								),
+							},
+
+							{
+								title: 'Quy cách',
+								render: (data: any) => <>{data?.specificationsUu?.name || '---'}</>,
+							},
+							{
+								title: 'KL 1 (Tấn)',
+								render: (data: any) => <>{convertWeight(data?.weigth1)}</>,
+							},
+							{
+								title: 'KL 2 (Tấn)',
+								render: (data: any) => <>{convertWeight(data?.weigth2)}</>,
+							},
+							{
+								title: 'KL tươi (Tấn)',
+								render: (data: any) => <>{convertWeight(data?.weightTotal) || 0}</>,
+							},
+							{
+								title: 'Đến',
+								render: (data: any) => (
+									<>
+										<p style={{marginBottom: 4, fontWeight: 600}}>{data?.toUu?.name || '---'}</p>
+										<p style={{fontWeight: 600, color: '#3772FF'}}>
+											{data?.batchsUu?.shipOutUu?.licensePalate || '---'}
+										</p>
+									</>
+								),
+							},
+							{
+								title: 'Ngày bắt đầu',
+								render: (data: any) => (
+									<>{data?.timeStart ? <Moment date={data?.timeStart} format='DD/MM/YYYY' /> : '---'}</>
+								),
+							},
+							{
+								title: 'Ngày kết thúc',
+								render: (data: any) => <>{data?.timeEnd ? <Moment date={data?.timeEnd} format='DD/MM/YYYY' /> : '---'}</>,
+							},
+
+							{
+								title: 'Tác vụ',
+								fixedRight: true,
+								render: (data: any) => (
+									<div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
+										<IconCustom
+											edit
+											icon={<LuPencil fontSize={20} fontWeight={600} />}
+											tooltip='Chỉnh sửa xuất thẳng'
+											color='#777E90'
+											href={`/nhap-xuat-ngoai/chinh-sua-xuat-thang?_uuid=${data.uuid}`}
+										/>
+
+										<IconCustom
+											edit
+											icon={<Eye size={22} fontWeight={600} />}
+											tooltip='Xem chi tiết'
+											color='#777E90'
+											href={`/nhap-xuat-ngoai/${data.uuid}`}
+										/>
+									</div>
+								),
+							},
+						]}
+					/>
+				</DataWrapper>
+
+				<Pagination
+					currentPage={Number(_page) || 1}
+					pageSize={Number(_pageSize) || 50}
+					total={ListBill?.data?.pagination?.totalCount}
+					dependencies={[_pageSize, _keyword, _dateFrom, _dateTo, _isBatch]}
+				/>
+			</div>
+		</div>
+	);
+}
 
 export default MainPageDirect;
