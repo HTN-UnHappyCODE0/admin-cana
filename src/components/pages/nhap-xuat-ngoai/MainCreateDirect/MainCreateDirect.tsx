79bd3427977de631a8f5aff94e3c3d97b6145221
TuanVT <vutrongtuan@gmail.com>
2024-11-08 08:26:22 +0700

    ff

--- src/components/pages/nhap-xuat-ngoai/MainCreateDirect/MainCreateDirect.tsx
+++ src/components/pages/nhap-xuat-ngoai/MainCreateDirect/MainCreateDirect.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import {PropsMainCreateDirect} from './interfaces';
 import styles from './MainCreateDirect.module.scss';
+import {useRouter} from 'next/router';
 
 function MainCreateDirect({}: PropsMainCreateDirect) {
+	const router = useRouter();
 	return <div>MainCreateDirect</div>;
 }
 
